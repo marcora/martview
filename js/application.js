e81491eb93f1356582ac3d7aa6a610ebce1aab1e
Edoardo "Dado" Marcora <edoardo.marcora@gmail.com>
2009-08-03 15:03:22 +0100

    Build change form menu based of query params

--- js/application.js
+++ js/application.js
@@ -12,10 +12,64 @@ Martview.Application = function (params) {
     var changeform = Ext.getCmp('changeform');
     var attributes = new Martview.Attributes();
 
-    console.dir(params);
+    var current_server, current_query, current_form;
+    if (params.server) {
+        current_server = params.server;
+        if (params.query) {
+            current_query = params.query;
+            if (params.form) {
+                current_form = params.form;
+            }
+        }
+    }
 
-    // private methods
-    var changeDataset = function (menu_item) {
+    // populate the 'Choose database' menu button, should go into database init
+    var init = function () {
+        var url;
+        if (current_server) {
+            url = './json/' + params.server + '.datasets.json';
+        } else {
+            url = './json/marts_and_datasets.json';
+        }
+        conn.request({
+            url: url,
+            success: function (response) {
+                var data = Ext.util.JSON.decode(response.responseText);
+                if (current_server) {
+                    if (current_query) {
+                        if (current_form) {
+                            // server + query + form params
+                        } else {
+                            // server + query params
+                        }
+                    } else {
+                        // server param
+                        // build menu from json data
+                        changeform.menu.add(data);
+                        // add changeForm handler to each dataset menu item
+                        changeform.menu.items.each(function (dataset) {
+                            dataset.menu.on('itemclick', changeForm);
+                        });
+                    }
+                } else {
+                    // build menu from json data
+                    changeform.menu.add(data);
+                    // add changeForm handler to each dataset menu item
+                    changeform.menu.items.each(function (mart) {
+                        mart.menu.items.each(function (dataset) {
+                            dataset.menu.on('itemclick', changeForm);
+                        });
+                    });
+                }
+            },
+            failure: function () {
+                Ext.Msg.alert(Martview.APP_TITLE, 'Unable to connect to the BioMart service.');
+            }
+        });
+    } ();
+
+    // event handlers
+    var changeForm = function (menu_item) {
         query.getTopToolbar().items.each(function (item) {
             item.enable();
         });
@@ -58,24 +112,7 @@ Martview.Application = function (params) {
         //.setText(menu_item.parentMenu.getText() + ' &sdot; ' + menu_item.getText());
     };
 
-    //populate the 'Choose database' menu button, should go into database init
-    conn.request({
-        url: './json/marts_and_datasets.json',
-        success: function (response) {
-            var data = Ext.util.JSON.decode(response.responseText);
-            changeform.menu.add(data);
-            // add changeDataset handler to each dataset menu item
-            changeform.menu.items.each(function (mart) {
-                mart.menu.items.each(function (dataset) {
-                    dataset.menu.on('itemclick', changeDataset);
-                });
-            });
-        },
-        failure: function () {
-            Ext.Msg.alert(Martview.APP_TITLE, 'Unable to connect to the BioMart service.');
-        }
-    });
-
+    // event listeners
     query.getTopToolbar().items.get('run').on('click', function () {
 
         var results_data = [['NeuroD', 71.72, 0.02, 0.03, '9/1 12:00am'], ['Huntingtin', 29.01, 0.42, 1.47, '9/1 12:00am'], ['HAP1', 83.81, 0.28, 0.34, '9/1 12:00am'], ['CaMKII', 52.55, 0.01, 0.02, '9/1 12:00am'], ['PSD-95', 64.13, 0.31, 0.49, '9/1 12:00am'], ['NF-kappa B', 31.61, -0.48, -1.54, '9/1 12:00am'], ['p65', 75.43, 0.53, 0.71, '9/1 12:00am'], ['p50', 67.27, 0.92, 1.39, '9/1 12:00am'], ['NR1', 49.37, 0.02, 0.04, '9/1 12:00am'], ['NR2A', 40.48, 0.51, 1.28, '9/1 12:00am'], ['NR2B', 68.1, -0.43, -0.64, '9/1 12:00am'], ['synGAP', 34.14, -0.08, -0.23, '9/1 12:00am'], ['RasGRF1', 30.27, 1.09, 3.74, '9/1 12:00am'], ['MyoD', 36.53, -0.03, -0.08, '9/1 12:00am'], ['Myogenin', 38.77, 0.05, 0.13, '9/1 12:00am'], ['Myf5', 19.88, 0.31, 1.58, '9/1 12:00am'], ['Neurogenin', 81.41, 0.44, 0.54, '9/1 12:00am'], ['Shank', 64.72, 0.06, 0.09, '9/1 12:00am'], ['Homer', 45.73, 0.07, 0.15, '9/1 12:00am'], ['Stargazin', 36.76, 0.86, 2.40, '9/1 12:00am'], ['Ras', 40.96, 0.41, 1.01, '9/1 12:00am'], ['I-kappa B', 25.84, 0.14, 0.54, '9/1 12:00am'], ['IKK', 27.96, 0.4, 1.45, '9/1 12:00am'], ['Dynein', 45.07, 0.26, 0.58, '9/1 12:00am'], ['MAP2', 61.91, 0.01, 0.02, '9/1 12:00am'], ['Erk1', 61.91, 0.01, 0.02, '9/1 12:00am'], ['Erk2', 61.91, 0.01, 0.02, '9/1 12:00am'], ['GFAP', 61.91, 0.01, 0.02, '9/1 12:00am'], ['N-CAM', 61.91, 0.01, 0.02, '9/1 12:00am'], ['Tubulin', 61.91, 0.01, 0.02, '9/1 12:00am'], ['Dynactin', 34.64, 0.35, 1.02, '9/1 12:00am'], ['Tubulin', 61.91, 0.01, 0.02, '9/1 12:00am'], ['Actin', 63.26, 0.55, 0.88, '9/1 12:00am']];
@@ -167,10 +204,8 @@ Ext.BLANK_IMAGE_URL = './ext/resources/images/default/s.gif';
 Ext.onReady(function () {
     Ext.QuickTips.init();
     Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+    // extract params from query string
     var params = Ext.urlDecode(window.location.search.substring(1));
-    new Martview.Application({
-        server: params.server,
-        query: params.query,
-        form: params.form
-    });
+    // create and init app object
+    new Martview.Application(params);
 });
